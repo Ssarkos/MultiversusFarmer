fe15b1e7b80e1e53d6089a4a69d5a3f80cb952c9
Ssarkos <81178115+Ssarkos@users.noreply.github.com>
2022-08-14 06:47:02 -0700

    Add files via upload

---
+++ MultiversusAfk/MultiversusAfk/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace MultiversusAfk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblChar = new System.Windows.Forms.Label();
+            this.lblGames = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnChar = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(82, 99);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(92, 27);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(49, 13);
+            this.lblTime.TabIndex = 9;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // lblChar
+            // 
+            this.lblChar.AutoSize = true;
+            this.lblChar.Location = new System.Drawing.Point(88, 28);
+            this.lblChar.Name = "lblChar";
+            this.lblChar.Size = new System.Drawing.Size(53, 13);
+            this.lblChar.TabIndex = 7;
+            this.lblChar.Text = "Character";
+            // 
+            // lblGames
+            // 
+            this.lblGames.AutoSize = true;
+            this.lblGames.Location = new System.Drawing.Point(70, 47);
+            this.lblGames.Name = "lblGames";
+            this.lblGames.Size = new System.Drawing.Size(95, 13);
+            this.lblGames.TabIndex = 8;
+            this.lblGames.Text = "Matches Played: 0";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(82, 99);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnChar
+            // 
+            this.btnChar.FormattingEnabled = true;
+            this.btnChar.Items.AddRange(new object[] {
+            "Random",
+            "Arya Stark",
+            "Batman",
+            "Bugs Bunny",
+            "Finn",
+            "Garnet",
+            "Harley Quinn",
+            "Iron Giant",
+            "Jake",
+            "Lebron James",
+            "Reindog",
+            "Shaggy",
+            "Steven Universe",
+            "Superman",
+            "Taz",
+            "Tom & Jerry",
+            "Velma",
+            "Wonder Woman"});
+            this.btnChar.Location = new System.Drawing.Point(55, 44);
+            this.btnChar.MaxDropDownItems = 20;
+            this.btnChar.Name = "btnChar";
+            this.btnChar.Size = new System.Drawing.Size(121, 21);
+            this.btnChar.TabIndex = 6;
+            this.btnChar.Text = "Random";
+            this.btnChar.SelectedIndexChanged += new System.EventHandler(this.btnChar_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 145);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblChar);
+            this.Controls.Add(this.lblGames);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnChar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblChar;
+        private System.Windows.Forms.Label lblGames;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ComboBox btnChar;
+    }
+}
+
---
+++ MultiversusAfk/MultiversusAfk/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace MultiversusAfk
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint wMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, int lParam);
+
+
+        const uint WM_KEYDOWN = 0x100;
+        const uint WM_KEYUP = 0x0101;
+        const UInt32 WM_CHAR = 0x0102;
+
+        IntPtr V = (IntPtr)0x56;
+        IntPtr W = (IntPtr)0x57;
+        IntPtr A = (IntPtr)0x41;
+        IntPtr S = (IntPtr)0x53;
+        IntPtr D = (IntPtr)0x44;
+        IntPtr O = (IntPtr)0x4F;
+        IntPtr T = (IntPtr)0x54;
+        IntPtr P = (IntPtr)0x50;
+        IntPtr Space = (IntPtr)0x20;
+        IntPtr VKEnter = (IntPtr)0x0D;
+
+        bool running = false;
+        ThreadStart run;
+        Thread farming;
+        ThreadStart time;
+        Thread updateTimer;
+        int gamesPlayed = 0;
+        int stepsX = 0;
+        int stepsY = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {                
+                running = true;
+                run = new ThreadStart(farm);
+                farming = new Thread(run);
+                farming.Start();
+
+                farmMenu();
+            }
+        }
+
+        private void idleMenu()
+        {
+            btnStart.Show();
+            btnChar.Show();
+            lblChar.Show();
+
+            btnStop.Hide();
+            lblGames.Hide();
+            lblTime.Hide();
+        }
+
+        private void farmMenu()
+        {
+            btnStart.Hide();
+            btnChar.Hide();
+            lblChar.Hide();
+
+            btnStop.Show();
+            lblGames.Show();
+            lblTime.Show();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            running = false;
+            farming.Abort();
+            updateTimer.Abort();
+            gamesPlayed = 0;
+
+            idleMenu();
+        }
+
+        private void timer()
+        {
+            DateTime start = DateTime.Now;
+            while (true)
+            {
+                TimeSpan time = TimeSpan.FromSeconds((DateTime.Now - start).TotalSeconds);
+                lblTime.Text = time.ToString(@"hh\:mm\:ss");
+                Thread.Sleep(50);
+            }
+        }
+
+        private void farm()
+        {
+            IntPtr hwnd = FindWindow(null, "MultiVersus  ");
+
+            if (hwnd == IntPtr.Zero)
+            {
+                MessageBox.Show("MultiVersus is not running.", "Error");
+                running = false;
+                idleMenu();
+
+                return;
+            }
+
+            time = new ThreadStart(timer);
+            updateTimer = new Thread(time);
+            updateTimer.Start();
+
+            while (true)
+            {
+                //for (int j = 0; j < 2; j++)
+                //{
+                    lblGames.Text = "Matches Played: " + gamesPlayed;                   
+
+                    Thread.Sleep(1000);
+                    for (int i = 0; i < 17; i++)
+                    {
+                        presskey(S, hwnd);
+                        presskey(D, hwnd);
+                        Thread.Sleep(30);
+                    }
+
+                    for (int i = 0; i < stepsX; i++)
+                    {
+                        presskey(A, hwnd);
+                        Thread.Sleep(30);
+                    }
+
+                    for (int i = 0; i < stepsY; i++)
+                    {
+                        presskey(W, hwnd);
+                        Thread.Sleep(30);
+                    }
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        presskey(Space, hwnd);
+                    }
+                    Thread.Sleep(300);
+                    for (int i = 0; i < 4; i++)
+                    {
+                        presskey(V, hwnd);
+  
+                    }
+
+                    /*if (j == 0)
+                    {
+                        Thread.Sleep(17000);
+                    }
+                    else
+                    {
+                        Thread.Sleep(10000);
+                    }*/
+
+                    Thread.Sleep(16000);
+
+                    for (int i = 0; i < 20; i++)
+                    {
+                        presskey(V, hwnd);
+                        Thread.Sleep(100);
+                    }
+
+                    Thread.Sleep(31000);
+
+                    for (int i = 0; i < 7; i++)
+                    {
+                        presskey(O, hwnd);
+                        presskey(P, hwnd);
+                        Thread.Sleep(500);
+                        presskey(P, hwnd);
+                        Thread.Sleep(900);
+                    }
+
+                    for (int i = 0; i < 55; i++)
+                    {
+                        PostMessage(hwnd, WM_KEYDOWN, A, 0);
+                        presskey(P, hwnd);
+                        Thread.Sleep(500);
+                    }
+                    PostMessage(hwnd, WM_KEYUP, A, 0);
+
+                    Thread.Sleep(12000);
+
+                    for (int i = 0; i < 30; i++)
+                    {
+                        presskey(V, hwnd);
+                        presskey(Space, hwnd);
+                        Thread.Sleep(100);
+                    }
+
+                    Thread.Sleep(5000);
+
+                    gamesPlayed++;
+                //}
+            }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void presskey(IntPtr key, IntPtr hwnd)
+        {
+            PostMessage(hwnd, WM_KEYDOWN, key, 0);
+            Thread.Sleep(10);
+            PostMessage(hwnd, WM_KEYUP, key, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            idleMenu();
+        }
+
+        private void btnChar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if ((string)btnChar.SelectedItem == "Shaggy")
+            {
+                stepsX = 8;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Batman")
+            {
+                stepsX = 7;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Taz")
+            {
+                stepsX = 6;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Garnet")
+            {
+                stepsX = 5;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Jake")
+            {
+                stepsX = 4;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Finn")
+            {
+                stepsX = 3;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Arya Stark")
+            {
+                stepsX = 2;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Harley Quinn")
+            {
+                stepsX = 1;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Lebron James")
+            {
+                stepsX = 0;
+                stepsY = 1;
+            }
+            else if ((string)btnChar.SelectedItem == "Wonder Woman")
+            {
+                stepsX = 8;
+                stepsY = 0;
+            }
+            else if ((string)btnChar.SelectedItem == "Superman")
+            {
+                stepsX = 7;
+                stepsY = 0;
+            }
+            else if ((string)btnChar.SelectedItem == "Iron Giant")
+            {
+                stepsX = 6;
+                stepsY = 0;
+            }
+            else if ((string)btnChar.SelectedItem == "Steven Universe")
+            {
+                stepsX = 5;
+                stepsY = 0;
+            }
+            else if ((string)btnChar.SelectedItem == "Reindog")
+            {
+                stepsX = 4;
+                stepsY = 0;
+            }
+            else if ((string)btnChar.SelectedItem == "Velma")
+            {
+                stepsX = 3;
+                stepsY = 0;
+            }
+            else if ((string)btnChar.SelectedItem == "Bugs Bunny")
+            {
+                stepsX = 2;
+                stepsY = 0;
+            }
+            else if ((string)btnChar.SelectedItem == "Tom & Jerry")
+            {
+                stepsX = 1;
+                stepsY = 0;
+            }
+            else
+            {
+                stepsX = 0;
+                stepsY = 0;
+            }
+        }
+    }
+}
